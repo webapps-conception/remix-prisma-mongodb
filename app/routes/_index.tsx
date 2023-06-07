411d91dc6873926f30836409fcdca3f32abc621c
Rudy <rudy@paloma.tr>
2023-06-07 16:00:19 +0200

    Version 2.0.0: Authentication.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,9 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { LoaderFunction } from '@remix-run/node'
+import { requireUserId } from '~/utils/auth.server'
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return null
 };
 
 export default function Index() {
@@ -12,5 +11,5 @@ export default function Index() {
     <div className="h-screen bg-slate-700 flex justify-center items-center">
       <h2 className="text-blue-600 font-extrabold text-5xl">TailwindCSS Is Working!</h2>
     </div>
-  )
+  );
 }
